d940b2f0b08250d55008cffdd04f7c1e69dac1d3
Musfirah Lodhi <Musfirahlodhi4@gmail.com>
2024-03-31 08:40:12 +0500

    Assignment 19

---
+++ app.js
@@ -0,0 +1,79 @@
+// // Chap 26-----------------
+// // q1
+// var num = 5.55
+// var roundnum = Math.ceil(num);
+// console.log(roundnum);
+// // q2
+// var orignNum = 3.333;
+// roundNum = Math.ceil(orignNum);
+// console.log(roundNum);
+// // q3
+// var orignNum = 3.333;
+// roundNum = Math.floor(orignNum);
+// console.log(roundNum);
+// // q4
+// var orignNum = 3.333;
+// roundNum = Math.floor(orignNum);
+// console.log(roundNum);
+// // q5
+// var a = 0.5;
+// var b = Math.floor(a);
+// console.log(b);
+
+// // Chap 27------------
+// // q1
+// var randomNum = Math.random()
+// var roundoffRandomNum = Math.ceil(randomNum*50);
+// console.log(roundoffRandomNum);
+// // q2
+// var randomNum = Math.random();
+// console.log(randomNum);
+// // q3
+
+// var diceRollResult = Math.ceil(Math.random() * 6);
+// console.log("You rolled a " + diceRollResult);
+
+// // q4
+// var headUser = prompt("Enter your name");
+// var tailUser = prompt("Enter your name");
+// var toss = Math.random()*2;
+// var convert = Math.floor(toss);
+// if(convert===0){
+//     console.log(headUser+ "Won")
+// }else{
+//     console.log(tailUser + "Won")
+// }
+
+// Chap 28,29----------------
+// Q1
+//  parseFloat,parseInt
+// // q2
+// var string = "123"
+// var convert = parseInt(string);
+// console.log(convert);
+// // q3
+// var num = "3.45";
+// var convert = parseFloat(num);
+// console.log(num);
+// // q4
+// var num = "3.45";
+// var convert = parseFloat(num);
+// console.log(typeof num);
+// q5
+// by using Number.toString
+// // q6
+// var num = 42;
+// var convert = num.toString();
+// console.log(convert);
+// q7
+// var num = "3.14";
+// var convert = parseFloat(num);
+// console.log(num);
+
+// chap 30--------------------
+// // q1
+// var num = 3.8347854970809764;
+// var num1= num.toFixed(4);
+// var newNum = num1.toString();
+// console.log(newNum);
+
